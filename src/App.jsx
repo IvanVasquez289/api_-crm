3b6f6b67b6b7f0553696f76610b20be62a719f1f
Ivan Vasquez <ivanjv1234@gmail.com>
2022-06-13 17:07:51 -0500

    Feat: Add react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+function App() {
+
+
+  return (
+    <h1>CRM - React</h1>
+  )
+}
+
+export default App
